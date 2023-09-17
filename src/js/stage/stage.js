da17beafcbc43d415038e6870d808fe3c9663118
Sarah Brochard <sarah.brochard@semknox.com>
2023-09-17 18:25:43 +0200

    [game-dev]: A Trekkers Gears
    stage.js

---
+++ src/js/stage/stage.js
@@ -0,0 +1,11 @@
+
+/**
+ * @classdesc
+ * a default "Stage" object.
+ * every "stage" object (title screen, credits, ingame, etc...) to be managed
+ * through the state manager must inherit from this base class.
+ * @see state
+ */
+export default class Stage {
+
+};
